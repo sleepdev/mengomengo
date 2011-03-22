5532d196683269fcb4dd51987905474751375059
andrew <sleepdev@gmail.com>
2011-03-22 00:33:09 -0700

    Added packaging for favicon and robots.txt

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     packages=['app'],
     package_data={  
-        'app': ['static/*/*','views/*.html','views/*/*.html'],
+        'app': ['static/favicon.ico','static/robots.txt','static/*/*','views/*.html','views/*/*.html'],
     },
     scripts=['deploy'],
 )
