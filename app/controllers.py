a86a3a0eb3dce546fdfadf0c1617ca2066c165ce
andrew <sleepdev@gmail.com>
2011-03-23 00:13:38 -0700

    Rebuilding Feed

--- app/controllers.py
+++ app/controllers.py
@@ -1,7 +1,19 @@
 import tornado.web
+import tornado.database
+
+db = tornado.database(
+    host="localhost", database="mengomengo",
+    user="root", password="mengomengo", 
+)
 
 
-            
+
+class BaseHandler( tornado.web.RequestHandler ):
+    def current_user( self ):
+        return self.secure_cookie('user')
+
+
+    
 class index( tornado.web.RequestHandler ):
     def get( self ):
         self.redirect("/connect")
@@ -17,3 +29,5 @@ class connect( tornado.web.RequestHandler ):
 class myfeed( tornado.web.RequestHandler ):
     def get( self ):
         self.render("myfeed.html")
+
+
--- app/models.sql
+++ app/models.sql
@@ -1,28 +1,39 @@
 SET SESSION storage_engine = "InnoDB";
 SET SESSION time_zone = "+0:00";
+ALTER DATABASE CHARACTER SET "utf8";
 
 
-CREATE TABLE playlist (
-    owner VARBINARY(100) NOT NULL,
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fb_id VARBINARY(100) NOT NULL UNIQUE
+);
+CREATE TABLE user_playlist (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user INT NOT NULL REFERENCES(user),
     listname VARBINARY(200) NOT NULL,
     permissions ENUM('private','shared','public') NOT NULL,
-    PRIMARY KEY(user,listname)
+    UNIQUE KEY(user,listname)
 );
-
-CREATE TABLE video (
-    owner VARBINARY(100) NOT NULL,
-    listname VARBINARY(200) NOT NULL,
-    urlhash VARBINARY(100) NOT NULL,
+CREATE TABLE user_video (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user INT NOT NULL REFERENCES(user),
+    playlist INT NOT NULL REFERENCES(playlist),
     url VARBINARY(500) NOT NULL,
     title VARBINARY(500),
-    ep INT,
+    episode INT,
     part INT,
-    PRIMARY KEY(user,listname,urlhash)
+    KEY(user,user_playlist)
+);
+CREATE TABLE user_subscription( 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user INT NOT NULL REFERENCES(user)
+    interest INT NOT NULL,
+    playlist INT NOT NULL REFERENCES(user_playlist)
 );
 
 CREATE TABLE history (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user VARBINARY(100) NOT NULL,
+    user INT NOT NULL REFERENCES(user),
     ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     action VARBINARY(2000) NOT NULL,
     KEY(user,ts)
