db3a72f4568e5753535168ee0dd4da7c088fd46e
andrew <sleepdev@gmail.com>
2011-03-29 06:05:53 -0700

    Working on javascript for rest feed.

--- app/static/javascript/main.js
+++ app/static/javascript/main.js
@@ -15,39 +15,36 @@ function logout()
 /* sets up a facebook/twitter style get/post/del data feed */
 function rest_feed(options)
 {
-  var rest_feed = this;
-  var feed = options['feed'] || $('#feed');
-  var feed_form = options['feed_form'] || $('#feed_form');
-  var feed_submit = options['feed_submit'] || $('#feed_submit');
-  var feed_more = options['feed_more'] || $('#feed_more');
   var url = options['url'];
+  var feed_api = this;
+  var feed = options['feed'] || $('#feed');
   var template = options['template'] || '<li>{{text}}</li>';
   var start = 0;
   var limit = options['limit'] || 15;
 
-  this.render( json ) {
-    /* todo, fix to decide append or prepend */
+  this.render = function( json ) {
+    /* todo, fix to decide append or prepend order */
     var html = template;
     for( k in json ){
       html = html.replace('{{'+k+'}}',json[k])
     }
     var dom = $(html);
     dom.data("rest_data",data);
-    feed.append( dom );
+    feed.find('.list').append( dom );
     start = start + 1;
   }
   this.post = function( data ){
     var form_data = {}; 
-    feed_form.children('[name]').each(function(i){
+    feed.find('.form [name]').each(function(i){
       form_data[i.attr('name')] = i.attr('value');
     });
     $.ajax({
       type: 'POST',
       dataType: 'json',
       url: url,
-      data: form_data,
+      data: feed.find('.feedform'),
       success: function( data ){
-        rest_feed.render(data);
+        feed_api.render(data);
       }
     });    
   }
@@ -58,8 +55,8 @@ function rest_feed(options)
       url: url, 
       data: {start: start, limit: limit},
       success: function( list ){
-        $.each(list, rest_feed.render);
-        if( list.length < limit ) feed_more.hide();
+        $.each(list, feed_api.render);
+        if( list.length < limit ) feed.find('.more').hide();
       }
     });
   }
@@ -71,8 +68,8 @@ function rest_feed(options)
     });
     ui.slideUp();
   }
-  feed_submit.click(function(){ rest_feed.post(); });
-  feed_more.click(function(){ rest_feed.get(); });
+  feed_submit.click(function(){ feed_api.post(); });
+  feed_more.click(function(){ feed_api.get(); });
   this.get();
 }
 
