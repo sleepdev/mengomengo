9ec3fcc1abcf5b605da70323309a118a2b0f36f8
andrew <sleepdev@gmail.com>
2011-04-16 01:41:39 -0700

    Added regx to pull out matches

--- app/controllers.py
+++ app/controllers.py
@@ -11,7 +11,9 @@ db = tornado.database.Connection(
 re_fbid = re.compile("access_token=(?P<pk>[^&]*)")
 class BaseRequestHandler( tornado.web.RequestHandler ):
     def get_current_user( self ):
-	return re_fbid.match(self.get_cookie("fbs_204128796282802")).groups()[0]        
+        cookie = self.get_cookie("fbs_204128796282802")
+        fbid = self.get_cookie("fbs_204128796282802")        
+	return fbid.groups()[0]        
     def has_permission( self, verb, owner, object ):
         authorized = True
         if not authorized:
