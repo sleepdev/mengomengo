e5b0a109ae9650c22939e06edefd786b50ac365c
andrew <sleepdev@gmail.com>
2011-04-16 01:38:24 -0700

    Added regx to pull out matches

--- app/controllers.py
+++ app/controllers.py
@@ -8,7 +8,7 @@ db = tornado.database.Connection(
     user="root", password="mengomengo", 
 )
 
-re_fbid = re.compile("access_token=(?P<tk>.*)")
+re_fbid = re.compile("access_token=(?P<pk>[^&]*)")
 class BaseRequestHandler( tornado.web.RequestHandler ):
     def get_current_user( self ):
 	raise Exception( self.get_cookie("fbs_204128796282802") )        
